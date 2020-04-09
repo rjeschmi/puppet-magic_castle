6d230b9cc3dcacc8e5597b04d1db57393d4e63e2
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-04-09 17:39:20 -0400

    Refactor ipa_create_user to user python_freeipa
    Also add the ability to create home, project and scratch folders.

--- site/profile/files/freeipa/ipa_create_user.py
+++ site/profile/files/freeipa/ipa_create_user.py
@@ -1,74 +1,226 @@
 #!/usr/bin/env python
 import argparse
+import grp
 import os
+import pwd
+import shutil
+import stat
 import sys
 
-from ipalib import api, errors
-from ipalib.cli import cli
-from ipapython import ipautil
-from ipaplatform.paths import paths
+from glob import glob
 
-def init_api():
-    api.bootstrap_with_global_options(context='cli')
-    api.add_plugin(cli)
-    api.finalize()
-    api.Backend.cli.create_context()
+from selinux import chcon, security_getenforce
+from python_freeipa import ClientMeta
+from python_freeipa.exceptions import DuplicateEntry
 
-def user_add(uid, first, last, password, shell):
-    kargs = dict(uid=unicode(uid),
-                 givenname=unicode(first),
-                 sn=unicode(last),
-                 userpassword=unicode(password),
-                 loginshell=unicode(shell))
-    try:
-        uidnumber = os.stat('/mnt/home/' + uid).st_uid
-    except:
-        pass
-    else:
-        kargs['uidnumber'] = uidnumber
 
-    try:
-        return api.Command.user_add(**kargs)
-    except errors.DuplicateEntry:
-        return
-
-def group_add(name):
-    try:
-        return api.Command.group_add(name)
-    except errors.DuplicateEntry:
-        return
-
-def group_add_members(group, members):
-    api.Command.group_add_member(cn=unicode(group),
-                                 user=list(map(unicode, members)))
-
-def kinit(username, password):
-    ipautil.run([paths.KINIT, username], stdin=password+'\n')
-
-def kdestroy():
-    ipautil.run([paths.KDESTROY])
-
-def main(users, sponsor):
-    admin_passwd = os.environ['IPA_ADMIN_PASSWD']
-    guest_passwd = os.environ['IPA_GUEST_PASSWD']
-    init_api()
-    kinit('admin', admin_passwd)
+def add_users(client, users, password, shell, home_prefix):
     for user in users:
-        user_add(user, first=user, last=user, password=guest_passwd, shell='/bin/bash')
-    if sponsor:
-        group = u'def-' + sponsor
-        group_add(group)
-        group_add_members(group, users)        
-    kdestroy()
+        home_path = os.path.join(home_prefix, user)
+        user_input = {
+            "a_uid": user,
+            "o_givenname": user,
+            "o_sn": user,
+            "o_cn": user,
+            "o_loginshell": shell,
+            "o_random": True,
+        }
+        if os.path.exists(home_path):
+            stat_info = os.stat(home_path)
+            user_input["o_uidnumber"] = stat_info.st_uid
+            user_input["o_gidnumber"] = stat_info.st_gid
+        try:
+            user_info = client.user_add(**user_input)["result"]
+        except DuplicateEntry:
+            pass
+        else:
+            client.change_password(user, password, user_info["randompassword"])
 
-    # configure user password
+
+def add_groups(client, groups):
+    for group in groups:
+        try:
+            client.group_add(group)
+        except DuplicateEntry:
+            pass
+
+
+def add_users_to_groups(client, users, groups):
+    for group in groups:
+        client.group_add_member(a_cn=group, o_user=users)
+
+
+def create_home(users, home_prefix, selinux_enforce, selinux_context):
+    skel_files = glob("/etc/skel/*") + glob("/etc/skel/.*")
     for user in users:
-        kinit(user, "\n".join([guest_passwd]*3))
-        kdestroy()
+        home = os.path.join(home_prefix, user)
+        if not os.path.exists(home):
+            pwdnam = pwd.getpwnam(user)
+            uid = pwdnam.pw_uid
+            gid = pwdnam.pw_gid
+            os.mkdir(home)
+            os.chown(home, uid, gid)
+            os.chmod(home, stat.S_IRWXU)
+            if selinux_enforce:
+                chcon(home, selinux_context)
+            for src in skel_files:
+                filename = src.split("/")[-1]
+                dst = os.path.join(home, filename)
+                shutil.copy(src, dst)
+                os.chown(dst, uid, gid)
+                os.chmod(dst, stat.S_IRWXU)
+                if selinux_enforce:
+                    chcon(src, selinux_context)
 
-if __name__ == "__main__" :
-    parser = argparse.ArgumentParser(description='Add a batch of generic users with the same sponsor')
-    parser.add_argument('users', nargs='+', help='list of usernames to create')
-    parser.add_argument('--sponsor', help='name of the sponsor if any')
+
+def create_project(users, groups, project_prefix, selinux_enforce, selinux_context):
+    for group in groups:
+        gid = grp.getgrnam(group).gr_gid
+        project = os.path.join(project_prefix, str(gid))
+        if not os.path.exists(project):
+            os.mkdir(project)
+            os.chown(project, 0, gid)
+            os.chmod(project, stat.S_ISGID | stat.S_IRWXU | stat.S_IRWXG)
+            os.symlink(project, os.path.join(project_prefix, group))
+            if selinux_enforce:
+                chcon(project, selinux_context)
+        for user in users:
+            pwdnam = pwd.getpwnam(user)
+            uid = pwdnam.pw_uid
+            project_user = os.path.join(project_prefix, str(gid), user)
+            os.mkdir(project_user)
+            os.chown(project_user, uid, gid)
+            os.chmod(project_user, stat.S_ISGID | stat.S_IRWXU)
+            if selinux_enforce:
+                chcon(project, selinux_context)
+
+
+def create_scratch(users, scratch_prefix, selinux_enforce, selinux_context):
+    for user in users:
+        scratch = os.path.join(scratch_prefix, user)
+        if not os.path.exists(scratch):
+            pwdnam = pwd.getpwnam(user)
+            uid = pwdnam.pw_uid
+            gid = pwdnam.pw_gid
+            os.mkdir(scratch)
+            os.chown(scratch, uid, gid)
+            os.chmod(scratch, stat.S_IRWXU | stat.S_IRWXG)
+            if selinux_enforce:
+                chcon(scratch, selinux_context)
+
+
+def create_home_symlinks(
+    users,
+    groups,
+    home_prefix,
+    project_prefix,
+    scratch_prefix,
+    selinux_enforce,
+    selinux_context,
+):
+    for user in users:
+        home = os.path.join(home_prefix, user)
+        if os.path.exists(home):
+            scratch = os.path.join(scratch_prefix, user)
+            pwdnam = pwd.getpwnam(user)
+            uid, gid = pwdnam.pw_uid, pwdnam.pw_gid
+            home_projects = os.path.join(home, "projects")
+            os.mkdir(home_projects)
+            os.chown(home_projects, 0, gid)
+            os.chmod(home_projects, stat.S_IRWXU | stat.S_IRGRP | stat.S_IXGRP)
+            if selinux_enforce:
+                chcon(home_projects, selinux_context)
+            for group in groups:
+                project = os.path.join(project_prefix, group)
+                if os.path.exists(project):
+                    os.symlink(project, os.path.join(home_projects, group))
+            if os.path.exists(scratch):
+                os.symlink(scratch, os.path.join(home, "scratch"))
+
+
+def main(
+    admin_password,
+    users,
+    password,
+    groups,
+    shell,
+    chroot,
+    home_prefix,
+    project_prefix,
+    scratch_prefix,
+    enable_create_home,
+    enable_create_project,
+    enable_create_scratch,
+    enable_create_home_symlinks,
+    selinux_context,
+):
+    selinux_enforce = security_getenforce() == 1
+
+    client = ClientMeta(dns_discovery=True)
+    client.login("admin", admin_password)
+    add_users(client, users, password, shell, home_prefix)
+    add_groups(client, groups)
+    add_users_to_groups(client, users, groups)
+    client.logout()
+
+    if enable_create_home:
+        create_home(users, home_prefix, selinux_enforce, selinux_context)
+
+    if enable_create_project:
+        create_project(users, groups, project_prefix, selinux_enforce, selinux_context)
+
+    if enable_create_scratch:
+        create_scratch(users, scratch_prefix, selinux_enforce, selinux_context)
+
+    if enable_create_home_symlinks:
+        create_home_symlinks(
+            users,
+            groups,
+            home_prefix,
+            project_prefix,
+            scratch_prefix,
+            selinux_enforce,
+            selinux_context,
+        )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Create a batch of users in FreeIPA")
+    parser.add_argument("users", nargs="+", help="list of usernames")
+    parser.add_argument(
+        "--admin-password", nargs=1, default=os.environ.get("IPA_ADMIN_PASSWD")
+    )
+    parser.add_argument(
+        "--password", nargs=1, default=os.environ.get("IPA_USER_PASSWD")
+    )
+    parser.add_argument("--groups", nargs="*", help="list of groups")
+    parser.add_argument("--shell", default="/bin/bash")
+    parser.add_argument("--chroot", default="/")
+    parser.add_argument("--home-prefix", default="/home")
+    parser.add_argument("--project-prefix", default="/project")
+    parser.add_argument("--scratch-prefix", default="/scratch")
+    parser.add_argument("--create-home", action="store_true")
+    parser.add_argument("--create-project", action="store_true")
+    parser.add_argument("--create-scratch", action="store_true")
+    parser.add_argument("--create-home_symlinks", action="store_true")
+    parser.add_argument(
+        "--selinux-context", default="unconfined_u:object_r:user_home_t:s0"
+    )
+
     args = parser.parse_args()
-    main(args.users, args.sponsor)
+    main(
+        args.admin_password,
+        args.users,
+        args.password,
+        args.groups,
+        args.shell,
+        args.chroot,
+        args.home_prefix,
+        args.project_prefix,
+        args.scratch_prefix,
+        args.create_home,
+        args.create_project,
+        args.create_scratch,
+        args.create_home_symlinks,
+        args.selinux_context,
+    )
